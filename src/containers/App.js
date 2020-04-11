5d13e98238fb2fee4b8f9cec985a51fbdc5fd511
skabbe <lothloriell@yahoo.co.uk>
2020-04-11 17:21:58 +0200

    init commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,24 @@
 import React, {Fragment} from 'react'
+import  {connect} from 'react-redux';
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox.js'
 import Scroll from '../components/Scroll.js'
 import ErrorBoundry from '../components/ErrorBoundry.js'
 import './App.css'
+import  { setSearchField } from '../actions.js'
+
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchRobots.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 
 
 class App extends React.Component {
@@ -17,6 +32,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
+        console.log(this.props.store.getState())
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => this.setState({robots:users}))
@@ -49,4 +65,4 @@ class App extends React.Component {
 }
 
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App);
